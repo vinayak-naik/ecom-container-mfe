a2daef5292c7bf11c478ef8d1832ca2626388343
vinayak-naik <vinayaknaik2021@gmail.com>
2023-11-12 18:07:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LayoutMfe from 'layoutMfe/App'
+import AuthMfe from 'authMfe/App'
+
+function App() {
+
+  return (
+    <div><div>Container Micro Frontend</div>
+    <LayoutMfe/>
+    <AuthMfe/>
+    </div>
+  )
+}
+
+export default App
