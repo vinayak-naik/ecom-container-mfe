8351433c949aa18db0e6045e28abe0ed77c338dc
vinayak-naik <vinayaknaik2021@gmail.com>
2023-11-14 11:55:23 +0530

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+
+import { useState } from 'react'
 import './App.css'
+// import { AccountBalance } from '@mui/icons-material'
 import LayoutMfe from 'layoutMfe/App'
-import AuthMfe from 'authMfe/App'
+// import AuthMfe from 'authMfe/App'
 
 function App() {
+  const [name, setName] = useState("initial")
 
   return (
-    <div><div>Container Micro Frontend</div>
-    <LayoutMfe/>
-    <AuthMfe/> 
+    <div style={{background:"lightgrey"}}><div>Container Micro Frontend</div>
+     <LayoutMfe update={(e)=>setName(e)}/>
+    
+     <div>message:{name}</div>
     </div>
   )
 }
